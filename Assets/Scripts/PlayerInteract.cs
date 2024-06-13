0e0cbf1b9d4eb13c4a7e07672b54a9325f25b754
Κατερίνα Σφακιανού <57565765+katsfak@users.noreply.github.com>
2024-06-13 14:19:53 +0300

    Npc interaction

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    private List<Transform> npcList;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            float interactRange =2f;
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+            foreach (Collider collider in colliderArray) {
+                Debug.Log(collider);
+            }
+        }
+    }
+}

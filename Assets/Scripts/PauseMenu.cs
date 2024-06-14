03b90a26ce0cd17281c3be4c93a86a98e8c9efef
Μαρία Παπαδοπούλου <papadopoyloy.mary@hotmail.com>
2024-06-15 00:01:46 +0300

    pause menu again

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+  public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale=1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void Settings_button()
+   {
+        Time.timeScale=0f;
+        SceneManager.LoadScene("Settings");
+   }
+
+   public void Exit_button()
+   {
+        SceneManager.LoadScene("Exit");
+   }
+}

dce2c10fed2a2c5ca2e49952a0c10cadcce87d7a
Κατερίνα Σφακιανού <57565765+katsfak@users.noreply.github.com>
2024-06-19 02:16:36 +0300

    Shop DONE

--- Assets/Scripts/Shop Interaction/Button Info.cs
+++ Assets/Scripts/Shop Interaction/Button Info.cs
@@ -7,13 +7,11 @@ public class ButtonInfo : MonoBehaviour
 {
     public int ItemID;
     public Text PriceTxt;
-    public Text QuantityTxt;
     public GameObject ShopManager;
 
     private void Update()
     {
         ShopManager shopManagerScript = ShopManager.GetComponent<ShopManager>();
         PriceTxt.text = "Price: $" + shopManagerScript.shopItems[1, ItemID - 1].ToString();  // Adjusted to match the array index
-        QuantityTxt.text = shopManagerScript.shopItems[2, ItemID - 1].ToString();  // Adjusted to match the array index
     }
 }

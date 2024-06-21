f9bd044bdcca3c4129f9e703491f85508b2bc018
Κατερίνα Σφακιανού <57565765+katsfak@users.noreply.github.com>
2024-06-21 13:49:30 +0300

    Coins and Teleport to exit Dietitian Done

---
+++ Assets/Scripts/Dietitian/Exit Dietitian.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDietitian : MonoBehaviour
+{
+    [SerializeField] private Vector3 teleportPosition = new Vector3(-3.92f, 3.3f, -7.21f);
+    [SerializeField] private GameObject player;
+
+    public void TeleportPlayer()
+    {
+        StartCoroutine(Teleporter());
+    }
+
+    IEnumerator Teleporter()
+    {
+        yield return new WaitForSeconds(1);
+        player.transform.position = teleportPosition;
+    }
+}

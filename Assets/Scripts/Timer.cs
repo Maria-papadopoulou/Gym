930b9671f0c2451652ee05484c53faca20c6d0ef
Μαρία Παπαδοπούλου <papadopoyloy.mary@hotmail.com>
2024-06-24 20:39:44 +0300

    changed color of timer per 3 seconds

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using JetBrains.Annotations;
 
 public class Timer : MonoBehaviour
 {
-   [SerializeField] TextMeshProUGUI timerText;
-   float elapsedTime;
-   int minutes;
-   int seconds;
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
+    int minutes;
+    int seconds;
 
     // Update is called once per frame
     void Update()
     {
-        elapsedTime+= Time.deltaTime;
+        elapsedTime += Time.deltaTime;
         minutes = Mathf.FloorToInt(elapsedTime / 60);
         seconds = Mathf.FloorToInt(elapsedTime % 60);
 
-        timerText.text = string.Format("{0:00}:{1:00}",minutes, seconds);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        // Έλεγχος αν τα δευτερόλεπτα είναι πολλαπλάσιο του 3
+        if (seconds % 3 == 0 && seconds != 0)
+        {
+            timerText.color = Color.red; // Κάνουμε το χρώμα κόκκινο
+        }
+        else
+        {
+            timerText.color = Color.white; // Κάνουμε το χρώμα λευκό
+        }
     }
 }

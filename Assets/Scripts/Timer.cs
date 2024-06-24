d251768b5e9045d037b36ced5bc594c262cfb8e0
Μαρία Παπαδοπούλου <papadopoyloy.mary@hotmail.com>
2024-06-24 22:07:30 +0300

    cursor timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,31 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] Button toggleButton;
     float elapsedTime;
     int minutes;
     int seconds;
+    bool isRunning = true; // Αρχικά το χρονόμετρο είναι ενεργό
 
-    // Update is called once per frame
+    void Start()
+    {
+        // Σύνδεση της μεθόδου ToggleTimer με το κουμπί
+        toggleButton.onClick.AddListener(ToggleTimer);
+
+        // Αρχικά κλειδώνουμε τον κέρσορα για να είναι πάντα ορατός
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        // Αρχικοποίηση του χρονόμετρου και εμφάνιση του στο UI
+        UpdateTimerDisplay();
+    }
+
     void Update()
     {
-        elapsedTime += Time.deltaTime;
+        if (isRunning)
+        {
+            elapsedTime += Time.deltaTime;
+            UpdateTimerDisplay();
+
+            // Έλεγχος και αλλαγή χρώματος κειμένου κάθε 3 δευτερόλεπτα
+            UpdateTextColor();
+        }
+    }
+
+    void UpdateTimerDisplay()
+    {
         minutes = Mathf.FloorToInt(elapsedTime / 60);
         seconds = Mathf.FloorToInt(elapsedTime % 60);
-
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
 
-        // Έλεγχος αν τα δευτερόλεπτα είναι πολλαπλάσιο του 3
+    void UpdateTextColor()
+    {
         if (seconds % 3 == 0 && seconds != 0)
         {
-            timerText.color = Color.red; // Κάνουμε το χρώμα κόκκινο
+            timerText.color = Color.red;
         }
         else
         {
-            timerText.color = Color.white; // Κάνουμε το χρώμα λευκό
+            timerText.color = Color.white;
+        }
+    }
+
+    // Μέθοδος για εναλλαγή της κατάστασης του χρονομέτρου
+    public void ToggleTimer()
+    {
+        isRunning = !isRunning;
+
+        if (isRunning)
+        {
+            // Ξεκινάμε τον χρονόμετρο: καταστολή κέρσορα σε κατάσταση None (ορατός)
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            // Σταματάμε τον χρονόμετρο: καταστολή κέρσορα σε κατάσταση None (ορατός)
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
     }
 }

cb14fb46f2af0784bba77e004563d29689cc1990
Κατερίνα Σφακιανού <57565765+katsfak@users.noreply.github.com>
2024-07-05 00:29:37 +0300

    Fixes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,27 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void Exit_button() 
-   {
-        Application.Quit();
-        SceneManager.LoadScene("Exit");
-
-   }
-   public void Load_MainMenu()
-   {
-     SceneManager.LoadScene("Main Menu");
-   }
+     public void Exit_button() 
+     {
+          Application.Quit();
+          SceneManager.LoadScene("Exit");
+     }
+     public void Load_MainMenu()
+     {
+          SceneManager.LoadScene("Main Menu");
+     }
      public void Resume_button()
      {
           SceneManager.LoadScene("Game");
      }
-   
      public void Settings_button()
-   {
-        SceneManager.LoadScene("Settings");
-   }
-   public void Start_game()
-   {
-        SceneManager.LoadScene("Character Selection");
-   }
+     {
+          SceneManager.LoadScene("Settings");
+     }
+     public void Start_game()
+     {
+          SceneManager.LoadScene("Character Selection");
+     }
+     public void Online_Help()
+     {
+          SceneManager.LoadScene("Online Help");
+     }
 }
---
+++ Assets/Scripts/OnlineHelp.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnlineHelp : MonoBehaviour
+{
+    public void Done_button()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+   
+}
